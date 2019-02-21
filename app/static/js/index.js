79afb073aee1fe95a7d3e01a762bc4e3a32dc45b
JoostSijm <joostsijm@gmail.com>
2019-02-21 22:47:46 +0100

    Add webpack, improve generated pages

---
+++ app/static/js/index.js
@@ -0,0 +1,9 @@
+// import 'bootstrap';
+
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+console.log("Hello from JavaScript!");
+
+var name = "Bob", time = "today";
+console.log(`Hello ${name}, how are you ${time}?`);
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+	entry: './app/static/js/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'app/static/js'),
+		publicPath: 'static/js'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+           $: "jquery",
+           jQuery: "jquery"
+       })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        }
+      ]
+    }
+};
