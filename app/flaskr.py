2fc43e07e5ccfca0d3e556e24944d60e681d80b9
JoostSijm <joostsijm@gmail.com>
2019-03-01 18:07:27 +0100

    Catch 401 and 404 error

--- app/flaskr.py
+++ app/flaskr.py
@@ -3,6 +3,8 @@
 Simple flask thing
 """
 
+from flask import redirect, flash, render_template
+
 from app import app
 from app.modules.static import Static
 from app.modules.backend import Backend
@@ -17,3 +19,14 @@ app.register_blueprint(Backend, url_prefix='/backend')
 app.register_blueprint(Backend_Page, url_prefix='/backend/page')
 app.register_blueprint(Backend_File, url_prefix='/backend/file')
 app.register_blueprint(Backend_User, url_prefix='/backend/user')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("error/404.html")
+
+
+@app.errorhandler(401)
+def login_required(error):
+    flash('Please login before opening page', 'warning')
+    return redirect('/login', code=302)
