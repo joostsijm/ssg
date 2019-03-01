92b45258e7ba82e63e9afe674cfb0e6dd7dfa22c
JoostSijm <joostsijm@gmail.com>
2019-03-01 17:29:02 +0100

    Show 401 error if page exists

--- app/modules/static/app.py
+++ app/modules/static/app.py
@@ -3,6 +3,7 @@
 Serve static content
 """
 
+import os
 from flask import render_template, Blueprint, abort
 from jinja2 import TemplateNotFound
 from flask_login import current_user
@@ -18,9 +19,14 @@ BLUEPRINT = Blueprint(
 @BLUEPRINT.route("/<path:page>")
 def show(page):
     """Display static page"""
-    try:
-        if current_user.is_authenticated:
+    if current_user.is_authenticated:
+        try:
             return render_template("private/%s.html" % page)
+        except TemplateNotFound:
+            abort(404)
+    try:
         return render_template("public/%s.html" % page)
     except TemplateNotFound:
+        if os.path.exists("app/modules/static/pages/private/%s.html" % page):
+            abort(401)
         abort(404)
